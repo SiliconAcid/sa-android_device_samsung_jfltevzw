b74f1a2aa9d2e3e897047d254a0bb48b269449e7
liv3dn8as <twcoplien@gmail.com>
2014-12-30 11:57:19 -0600

    bringing up to 12.0
    deleted some whitespace and uncommonized loki

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@
 TARGET_OTA_ASSERT_DEVICE := jfltevzw
 
 # Kernel
-TARGET_KERNEL_CONFIG         := jf_vzw_defconfig
+TARGET_KERNEL_CONFIG := jf_vzw_defconfig
 
 # loki
-TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jf-common/loki
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jfltevzw/releasetools
